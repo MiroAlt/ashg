3936f08161ca3377f0c0de25c074fabda1dbc71f
Miro Alt <you@example.com>
2018-08-05 22:43:09 +0200

    Styling footer

--- app/js/main.js
+++ app/js/main.js
@@ -29,15 +29,58 @@ function includeHTML() {
 
 /* Sponsors */
 
+
+
 $(document).ready(function(){
-    $('.your-class').slick({
+    $('.js_sponsors').slick({
+        dots: false,
+        arrows: false,
         infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
+        autoplay: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><span class=\"icon\">\n" +
+        "        <svg role=\"img\" class=\"svg-icon\">\n" +
+        "            <use xlink:href=\"img/svg-symbols.svg#arrow-left2\"></use>\n" +
+        "        </svg>\n" +
+        "    </span></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><span class=\"icon\">\n" +
+        "        <svg role=\"img\" class=\"svg-icon\">\n" +
+        "            <use xlink:href=\"img/svg-symbols.svg#arrow-right2\"></use>\n" +
+        "        </svg>\n" +
+        "    </span></button>",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 });
-
-
+/*
 $(document).ready(function() {
     var $menu = $('#menu'),
         $menulink = $('.menu-link');
@@ -64,4 +107,4 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera
     document.documentElement.scrollTop = 0; // For IE and Firefox
-}
+}*/
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var changed = require('gulp-changed');
 var plumber = require('gulp-plumber');
 var cleancss = require('gulp-clean-css');
 var less = require('gulp-less');
+var imagemin = require('gulp-imagemin');
+var svgSymbols = require('gulp-svg-symbols');
 var browserSync = require('browser-sync').create();
 var SRC = 'app/js/*.js';
 var DEST = 'dist/js';
@@ -28,12 +30,37 @@ gulp.task('less', function () {
         .pipe(browserSync.stream());
 });
 
+/*Img min */
+gulp.task('img', function () {
+    return gulp.src('app/img/source/*')
+        .pipe(imagemin(
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ))
+        .pipe(gulp.dest('app/img'));
+});
+
+/*SVG sprites*/
+gulp.task('sprites', function () {
+
+    return gulp.src('app/img/svg/*.svg')
+        .pipe(svgSymbols({
+            svgClassname: 'svg-icon'
+        }))
+        .pipe(gulp.dest('app/img'));
+});
 
 /* Minify css */
 gulp.task('cleancss', function () {
 
     return gulp.src('app/css/*.css')
-
         .pipe(cleancss({
             keepSpecialComments: 1
         }))
@@ -51,9 +78,9 @@ gulp.task('changed', function () {
 /*jshint*/
 gulp.task('jshint', function () {
     gulp.src('app/js/**/*.js')
-    .pipe(plumber())
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+        .pipe(plumber())
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
 
@@ -61,8 +88,5 @@ gulp.task('watch', function () {
     gulp.watch(SRC, ['cleancss'])
 });
 
-
-
-
-
-
+// Default task
+gulp.task('default', ['serve','less', 'watch']);
