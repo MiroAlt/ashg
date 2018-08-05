e0a9cb71daadca3b412a32cd4000c8622b68d4d5
Miro Alt <you@example.com>
2018-08-05 20:30:13 +0200

    Initial setup

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,43 @@
+/* Inlcude content */
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("include-html");
+        if (file) {
+            /*make an HTTP request using the attribute value as the file name:*/
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                    /*remove the attribute, and call this function once more:*/
+                    elmnt.removeAttribute("include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /*exit the function:*/
+            return;
+        }
+    }
+}
+
+/* Sponsors */
+
+$(document).ready(function(){
+    $('.your-class').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+});
+
+
 $(document).ready(function() {
     var $menu = $('#menu'),
         $menulink = $('.menu-link');
